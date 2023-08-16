7af03974544c047f104cf88be70ede4b4d774477
Your GitHub Usernamemsdubova <femme777@gmail.com>
2023-08-16 22:47:35 +0100

    First test - calculate score of anylength number array

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1,27 +1,9 @@
 import { calculateGameScore } from '../index';
 
 describe('calculateGameScore', () => {
-  it('should calculate the score for a game with all open frames', () => {
-    const gameResult: (number | string)[] = [1, 4, 4, 5, 6, 4, 5, 5, 10, 0, 1, 7, 3, 6, 4, 10, 2, 8, 6];
+  it('should calculate the score', () => {
+    const gameResult: (number | string)[] = [1, 4, 4];
     const score = calculateGameScore(gameResult);
-    expect(score).toBe(133);
+    expect(score).toBe(9);
   });
-
-  it('should calculate the score for a game with spares and strikes', () => {
-    const gameResult: (number | string)[] = [5, 5, 5, 5, 10, 4, 6, 10, 10, 10, 4, 6, 8, 2, 10, 9, 1, 10];
-    const score = calculateGameScore(gameResult);
-    expect(score).toBe(201);
-  });
-
-  it('should calculate the score for a game with all strikes', () => {
-    const gameResult: (number | string)[] = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X'];
-    const score = calculateGameScore(gameResult);
-    expect(score).toBe(300);
-  });
-
-  it('should calculate the score for a game with a mix of spares, strikes, and open frames', () => {
-    const gameResult: (number | string)[] = ['X', 9, '/', 5, '/', 'X', 'X', 'X', 7, 2, 'X', 9, 0, 8, '/', 'X', 9, 1];
-    const score = calculateGameScore(gameResult);
-    expect(score).toBe(197);
-  });
-});
+})
