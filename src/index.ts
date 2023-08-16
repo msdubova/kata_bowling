27eb28f10895d8d343a9d9e3c95a11a4ca7207ef
Your GitHub Usernamemsdubova <femme777@gmail.com>
2023-08-17 00:04:50 +0100

    First test - array of numbers

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,26 @@
 // Create a function, which will get parameters - array of elements(numbers or strings) - result of each frame of the game  and will return number  - score of the game
+type Number = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
+type String = 'X' | '/' | '-';
+export type Trial = Number | String;
+type Frame = [Trial, Trial]
+export type Line = [Frame, Frame, Frame, Frame, Frame] | [Frame, Frame, Frame, Frame, Frame, Frame]
 
-export function calculateGameScore(frames: (number | string)[]): number {
-    return frames.reduce((acc: number, currentValue: number | string) => {
-        if (typeof currentValue === 'number') {
-            return acc + currentValue;
+export function calculateGameScore(line: Line): number {
+    return line.reduce((acc: number, currentValue: Frame) => {
+        const [firstTry, secondTry] = currentValue;
+        if (typeof firstTry === "number" && typeof secondTry === "number") {
+            if (firstTry + secondTry > 10) {
+                throw new Error(`Max 10 points , you try to strike ${firstTry} and ${secondTry}`)
+            } else {
+                acc += firstTry;
+                acc += secondTry;
+                console.log(`frame score ${firstTry} + ${secondTry}`)
+            }
         }
+
+
         return acc;
     }, 0);
 }
+
 
--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1,9 +1,9 @@
-import { calculateGameScore } from '../index';
+import { calculateGameScore, Trial, Line } from '../index';
 
 describe('calculateGameScore', () => {
-  it('should calculate the score', () => {
-    const gameResult: (number | string)[] = [1, 4, 4];
-    const score = calculateGameScore(gameResult);
-    expect(score).toBe(9);
-  });
-})
+    it('should calculate the score if only numbers - no spares, no strikes', () => {
+        const gameResult: Line = [[1, 2], [3, 4], [5, 5], [1, 1], [5, 2]];
+        const score = calculateGameScore(gameResult);
+        expect(score).toBe(29); 
+    });
+});
