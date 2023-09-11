f670c6f97fca35b99ac175abec71eab5fba68397
Your GitHub Usernamemsdubova <femme777@gmail.com>
2023-09-11 02:01:52 +0100

    Tests still in process

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,121 @@
 // Create a function, which will get parameters - array of elements(numbers or strings) - result of each frame of the game  and will return number  - score of the game
-type Number = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
-type String = 'X' | '/' | '-';
+type Number = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
+const GAMELENGTH = 10;
+const LASTFRAME = 8;
+const BONUSFRAME = 9;
+// 'X' - strike, '/' - spare, '-' - miss
+type String = "X" | "/" | "-";
 export type Trial = Number | String;
-type Frame = [Trial, Trial]
-export type Line = [Frame, Frame, Frame, Frame, Frame] | [Frame, Frame, Frame, Frame, Frame, Frame]
+type Frame = [Trial, Trial?] | ["X"] | ["-"];
+export type Line =
+  | [Frame, Frame, Frame, Frame, Frame, Frame, Frame, Frame, Frame, Frame]
+  | [
+      Frame,
+      Frame,
+      Frame,
+      Frame,
+      Frame,
+      Frame,
+      Frame,
+      Frame,
+      Frame,
+      Frame,
+      Frame
+    ]
+  | [
+      Frame,
+      Frame,
+      Frame,
+      Frame,
+      Frame,
+      Frame,
+      Frame,
+      Frame,
+      Frame,
+      Frame,
+      Frame,
+      Frame
+    ];
 
 export function calculateGameScore(line: Line): number {
-    return line.reduce((acc: number, currentValue: Frame, currentIndex: number) => {
-        const [firstTry, secondTry] = currentValue;
-        if (typeof firstTry === "number" && typeof secondTry === "number") {
-            if (firstTry + secondTry > 10) {
-                throw new Error(`Max 10 points in each frame`);
-            } else if (firstTry + secondTry === 10) {
-                const nextFrameIndex = currentIndex + 1;
-                const nextFrame = line[nextFrameIndex];
-                const bonus = nextFrame[0];
+  return line.reduce(
+    (acc: number, currentValue: Frame, currentIndex: number) => {
+      let [firstTry, secondTry] = currentValue;
 
-                if (typeof bonus === 'number') {
-                    acc += 10 + bonus;
-                } else if (bonus === 'X') {
-                    acc += 10 + 10;
-                }
-            } else {
-                acc += firstTry+secondTry;
-             
-                console.log(`frame score ${firstTry} + ${secondTry}`);
-            }
+      if (line.length > GAMELENGTH && line[LASTFRAME + 1][0] === "X" && line[LASTFRAME + 2][0] !== "X") {
+        if (line[BONUSFRAME].length === 1) {
+          throw new Error(`Not enough trials in the last bonus frame`);
         }
+      }
 
-        return acc;
-    }, 0);
+      if (line.length === GAMELENGTH && line[LASTFRAME][1] === "/") {
+        if (line[BONUSFRAME].length === 2) {
+          throw new Error(`Just one bonus trial for BONUSFRAME spare, not two`);
+        }
+      }
+
+      if (firstTry === "-") {
+        firstTry = 0;
+      }
+
+      if (secondTry === "-") {
+        secondTry = 0;
+      }
+      if (
+        firstTry === 0 &&
+        secondTry === undefined &&
+        line.length !== GAMELENGTH
+      ) {
+        throw new Error(`standart frame is double trial even if its miss`);
+      }
+      if (typeof firstTry === "number" && typeof secondTry === "number") {
+        if (firstTry + secondTry > 10) {
+          throw new Error(`Max 10 points in each frame`);
+        } else {
+          acc += firstTry + secondTry;
+          //   console.log(`frame score ${firstTry} + ${secondTry}`);
+        }
+      } else if (firstTry === "X" && secondTry === undefined) {
+        const nextFrameIndex = currentIndex + 1;
+        if (nextFrameIndex < line.length) {
+          const nextFrameFirst = line[nextFrameIndex][0];
+          const nextFrameSecond = line[nextFrameIndex][1];
+          if (typeof nextFrameFirst === "number") {
+            if (nextFrameSecond === "/") {
+              acc += 10 + nextFrameFirst;
+            } else if (nextFrameSecond === "X") {
+              acc += 20;
+            }
+          }
+        } else {
+          acc += 10;
+        }
+      } else if (firstTry === 5 && secondTry === "/") {
+        const nextFrameIndex = currentIndex + 1;
+        if (nextFrameIndex <= line.length - 1) {
+          const nextFrame = line[nextFrameIndex];
+          if (typeof nextFrame[0] === "number") {
+            acc += 10 + nextFrame[0];
+          } else if (nextFrame[0] === "X") {
+            acc += 20;
+          }
+        } else {
+          acc += 10;
+        }
+      } else if (typeof firstTry === "number" && secondTry === undefined) {
+        acc += firstTry;
+      } else if (firstTry === "/" && secondTry === undefined) {
+        throw new Error(`No single frame spare`);
+      } else if (firstTry === "X" && secondTry === "X") {
+        throw new Error(`No two strikes in one frame`);
+      } else if (secondTry === "X") {
+        throw new Error(`Strike can be just in single frame`);
+      } else if (firstTry === "/" && secondTry === "/") {
+        throw new Error(`Spare can be once in the frame`);
+      }
+
+      return acc;
+    },
+    0
+  );
 }
--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1,39 +1,365 @@
-import { calculateGameScore, Trial, Line } from '../index';
+import { calculateGameScore, Trial, Line } from "../index";
 
-describe('calculateGameScore', () => {
-   
-    test('should calculate the score if only numbers - no spares, no strikes', () => {
-        const gameResult: Line = [[1, 2], [3, 4], [5, 4], [1, 1], [5, 2]];
-        const score = calculateGameScore(gameResult);
-        expect(score).toBe(28); 
-        
-        const gameResult1: Line = [[1, 2], [3, 4], [5, 1], [2, 3], [4, 5]];
-        const score1 = calculateGameScore(gameResult1);
-        expect(score1).toBe(30);
+describe("calculateGameScore", () => {
+  test("should calculate the score if only numbers - no spares, no strikes", () => {
+    const gameResult: Line = [
+      [1, 2],
+      [3, 4],
+      [5, 4],
+      [1, 1],
+      [5, 2],
+      [1, 2],
+      [3, 4],
+      [5, 4],
+      [1, 1],
+      [5, 2],
+    ];
+    const score = calculateGameScore(gameResult);
+    expect(score).toBe(56);
 
-        const gameResult2: Line = [[4, 3], [2, 1], [3, 2], [1, 4], [5, 1]];
-        const score2 = calculateGameScore(gameResult2);
-        expect(score2).toBe(26);
+    const gameResult1: Line = [
+      [1, 2],
+      [3, 4],
+      [5, 1],
+      [2, 3],
+      [4, 5],
+      [1, 2],
+      [3, 4],
+      [5, 1],
+      [2, 3],
+      [4, 5],
+    ];
+    const score1 = calculateGameScore(gameResult1);
+    expect(score1).toBe(60);
 
-        const gameResult3: Line = [[5, 4], [2, 2], [3, 3], [4, 1], [1, 5]];
-        const score3 = calculateGameScore(gameResult3);
-        expect(score3).toBe(30);
+    const gameResult2: Line = [
+      [4, 3],
+      [2, 1],
+      [3, 2],
+      [1, 4],
+      [5, 1],
+      [4, 3],
+      [2, 1],
+      [3, 2],
+      [1, 4],
+      [5, 1],
+    ];
+    const score2 = calculateGameScore(gameResult2);
+    expect(score2).toBe(52);
 
-        const gameResult4: Line = [[3, 6], [4, 5], [2, 4], [1, 3], [5, 2]];
-        const score4 = calculateGameScore(gameResult4);
-        expect(score4).toBe(35);
-    });
+    const gameResult3: Line = [
+      [5, 4],
+      [2, 2],
+      [3, 3],
+      [4, 1],
+      [1, 5],
+      [5, 4],
+      [2, 2],
+      [3, 3],
+      [4, 1],
+      [1, 5],
+    ];
+    const score3 = calculateGameScore(gameResult3);
+    expect(score3).toBe(60);
 
-    test('should throw error if sum in some frame is more than 10', () => {
-        const gameResult: Line = [[1, 2], [8, 4], [5, 4], [1, 1], [5, 2]];
-        const scoreFunction = () => calculateGameScore(gameResult);
-        expect(scoreFunction).toThrowError("Max 10 points in each frame");
-    });
+    const gameResult4: Line = [
+      [3, 6],
+      [4, 5],
+      [2, 4],
+      [1, 3],
+      [5, 2],
+      [3, 6],
+      [4, 5],
+      [2, 4],
+      [1, 3],
+      [5, 2],
+    ];
+    const score4 = calculateGameScore(gameResult4);
 
-    test('should calculate score with spare', () => {
-        const gameResult: Line = [[1, 2], [3, 4], [5, 5], [1, 1], [5, 2]];
-        const score = calculateGameScore(gameResult);
-        expect(score).toBe(30);
-    });
+    expect(score4).toBe(70);
 
+    const gameResult5: Line = [
+      ["-", 6],
+      [4, 5],
+      ["-", 4],
+      [1, 3],
+      [5, 2],
+      ["-", 6],
+      [4, 5],
+      ["-", 4],
+      [1, 3],
+      [5, 2],
+    ];
+    const score5 = calculateGameScore(gameResult5);
+
+    expect(score5).toBe(60);
+  });
+
+  test("should throw error if sum in some frame is more than 10", () => {
+    const gameResult: Line = [
+      [1, 2],
+      [8, 4],
+      [5, 4],
+      [1, 1],
+      [5, 2],
+      [1, 2],
+      [8, 4],
+      [5, 4],
+      [1, 1],
+      [5, 2],
+    ];
+    const scoreFunction = () => calculateGameScore(gameResult);
+    expect(scoreFunction).toThrowError("Max 10 points in each frame");
+  });
+
+  test("should calculate score with spare", () => {
+    const gameResult: Line = [
+      [1, 2],
+      [3, 4],
+      [5, "/"],
+      [1, 1],
+      [5, 2],
+      [1, 2],
+      [3, 4],
+      [5, "/"],
+      [1, 1],
+      [5, 2],
+    ];
+    const score = calculateGameScore(gameResult);
+    expect(score).toBe(60);
+  });
+
+  test("should calculate score with spare in the 10th frame", () => {
+    const gameResult: Line = [
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      [5, "/"],
+      [2],
+    ];
+    const score = calculateGameScore(gameResult);
+    expect(score).toBe(32);
+  });
+
+  test("return error - cant be spare if one element in frame - the additional frame", () => {
+    const gameResult: Line = [
+      [1, 2],
+      [3, 4],
+      [5, "/"],
+      [1, 1],
+      [5, "/"],
+      [1, 2],
+      [3, 4],
+      [5, "/"],
+      [1, 1],
+      [5, "/"],
+      ["/"],
+    ];
+    const scoreFunction = () => calculateGameScore(gameResult);
+    expect(scoreFunction).toThrowError("No single frame spare");
+  });
+
+  test("return error - cant be spare if one element in frame", () => {
+    const gameResult: Line = [
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      [1, 2],
+      [3, 4],
+      ["/"],
+      [1, 1],
+      [5, "/"],
+    ];
+    const scoreFunction = () => calculateGameScore(gameResult);
+    expect(scoreFunction).toThrowError("No single frame spare");
+  });
+
+  test("return error - double X", () => {
+    const gameResult: Line = [
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      ["X", "X"],
+      [3, 4],
+      [5, "/"],
+      [1, 1],
+      [5, "/"],
+    ];
+    const scoreFunction = () => calculateGameScore(gameResult);
+    expect(scoreFunction).toThrowError("No two strikes in one frame");
+  });
+
+  test("return error - secondTry X", () => {
+    const gameResult: Line = [
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      [4, "X"],
+      [3, 4],
+      [5, "/"],
+      [1, 1],
+      [5, "/"],
+    ];
+    const scoreFunction = () => calculateGameScore(gameResult);
+    expect(scoreFunction).toThrowError("Strike can be just in single frame");
+  });
+
+  test("return error - double spare", () => {
+    const gameResult: Line = [
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      ["/", "/"],
+      [3, 4],
+      [5, "/"],
+      [1, 1],
+      [5, "/"],
+    ];
+    const scoreFunction = () => calculateGameScore(gameResult);
+    expect(scoreFunction).toThrowError("Spare can be once in the frame");
+  });
+
+  test("return error - Bonus for last 5 strike - 1 frame from two trials  - not from one ", () => {
+    const gameResult: Line = [
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      [2, 2],
+      [3, 4],
+      [5, "/"],
+      [1, 1],
+      ["X"],
+      [5],
+    ];
+    const scoreFunction = () => calculateGameScore(gameResult);
+    expect(scoreFunction).toThrowError(
+      "Not enough trials in the last bonus frame"
+    );
+  });
+
+  test("return error - Bonus for last 5 spare - 1 frame from one trial  -not from two ", () => {
+    const gameResult: Line = [
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      [2, 2],
+      [3, 4],
+      [5, "/"],
+      [1, 1],
+      [5, "/"],
+      [5, 5],
+    ];
+    const scoreFunction = () => calculateGameScore(gameResult);
+    expect(scoreFunction).toThrowError(
+      "Just one bonus trial for 5 spare, not two"
+    );
+  });
+
+  test("return error - standart frame is double trial even if its miss ", () => {
+    const gameResult: Line = [
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      ["-"],
+      [3, 4],
+      [5, "/"],
+      [1, 1],
+      [5, 4],
+    ];
+    const scoreFunction = () => calculateGameScore(gameResult);
+    expect(scoreFunction).toThrowError(
+      "standart frame is double trial even if its miss"
+    );
+  });
+
+  test("should calculate a perfect game", () => {
+    const gameResult1: Line = [
+      ["X"],
+      ["X"],
+      ["X"],
+      ["X"],
+      ["X"],
+      ["X"],
+      ["X"],
+      ["X"],
+      ["X"],
+      ["X"],
+      ["X"],
+      ["X"],
+    ];
+    const score1 = calculateGameScore(gameResult1);
+    expect(score1).toBe(320);
+  });
+
+  test("should calculate a game with all 9s and misses", () => {
+    const gameResult2: Line = [
+      [9, "-"],
+      [9, "-"],
+      [9, "-"],
+      [9, "-"],
+      [9, "-"],
+      [9, "-"],
+      [9, "-"],
+      [9, "-"],
+      [9, "-"],
+      [9, "-"],
+    ];
+    const score2 = calculateGameScore(gameResult2);
+    expect(score2).toBe(90);
+  });
+
+  test("should calculate a game with all spares and a final 5", () => {
+    const gameResult3: Line = [
+      [5, "/"],
+      [5, "/"],
+      [5, "/"],
+      [5, "/"],
+      [5, "/"],
+      [5, "/"],
+      [5, "/"],
+      [5, "/"],
+      [5, "/"],
+      [5, "/"],
+      [5],
+    ];
+    const score3 = calculateGameScore(gameResult3);
+    expect(score3).toBe(155);
+  });
+
+  test("should calculate a custom game result", () => {
+    const gameResult4: Line = [
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      [1, 1],
+      ["-", 6],
+      [4, 5],
+      ["-", 4],
+      [1, 3],
+      [5, 2],
+    ];
+    const score4 = calculateGameScore(gameResult4);
+    expect(score4).toBe(40);
+  });
 });
